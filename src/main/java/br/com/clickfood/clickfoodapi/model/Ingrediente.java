a1bacc1c36e6a669b6b33a2df570b8364eb91219
Aurélio Vinícius França dos Santos <58404045+aureliovinifs@users.noreply.github.com>
2021-08-27 12:40:21 -0300

    Update Ingrediente.java

--- src/main/java/br/com/clickfood/clickfoodapi/model/Ingrediente.java
+++ src/main/java/br/com/clickfood/clickfoodapi/model/Ingrediente.java
@@ -18,7 +18,7 @@ import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name = "ingrediente", schema = "heroku_b4b0e6c1521538d")
+@Table(name = "ingrediente", schema = "heroku_9ad0506350e14e0")
 public class Ingrediente {
 
 	@Id
